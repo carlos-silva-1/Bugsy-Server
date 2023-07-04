b06a0f3e95211e479233a0fa1a89428bb5a9c9f1
carlos-silva-1 <134824320+carlos-silva-1@users.noreply.github.com>
2023-07-04 18:13:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
-// app.options('*', cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+app.use(cors({ origin: true, credentials: true }));
+// app.options('*', cors());
 
 app.use('/api/users', userRoutes);
 app.use('/api/issues', issueRoutes);
