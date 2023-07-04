7f7190bb79ec76ed0af850d9ba5dbafe39827194
carlos-silva-1 <134824320+carlos-silva-1@users.noreply.github.com>
2023-07-04 20:06:16 -0300

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 365 * 24 * 60 * 60 * 1000, // 30 days
-    domain: 'bugsy.onrender.com',
+    domain: 'bugsyclient.onrender.com',
   });
 };
 
