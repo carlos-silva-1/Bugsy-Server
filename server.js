b0d2bde096a771169db06d0f909664d6bc737044
carlos-silva-1 <134824320+carlos-silva-1@users.noreply.github.com>
2023-07-04 19:02:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 
