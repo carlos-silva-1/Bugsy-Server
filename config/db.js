3f43304b02d9bfcdd248258799ecb453abdd587b
Carlos Silva <134824320+carlos-silva-1@users.noreply.github.com>
2024-06-27 17:15:17 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, clientOptions);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
